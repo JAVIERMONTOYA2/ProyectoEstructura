0d788fe7ace475cfdfe50ddf9d6bcd3e5f9f4e92
velaskz <benjamin.velasquez.f@mail.pucv.cl>
2023-12-01 23:46:47 -0300

    Arreglo de atacarEnemigos y eliminación de la estructura proyectil

--- main.c
+++ main.c
@@ -29,13 +29,6 @@ typedef struct {
 } Punto;
 
 typedef struct{
-    Punto inicio;
-    Punto final;
-    Punto actual;
-    char* tipo;
-} Proyectil ;
-
-typedef struct{
     SDL_Texture* textura;
     char* tipo;
     int dmg;
@@ -538,6 +531,11 @@ void atacarEnemigos(List* listaTorretas, List* listaEnemigos, Jugador* jugador, 
                         curTorreta->angulo += 180;
                     }
 
+                    if (enemigoADisparar != NULL) {
+                        enemigoADisparar->vida -= curTorreta->dmg;
+                        // Asegurarse de que la vida no se vuelva negativa
+                        enemigoADisparar->vida = fmax(enemigoADisparar->vida, 0);
+                    }
 
                 }
                 curEnemigo = nextList(listaEnemigos);
